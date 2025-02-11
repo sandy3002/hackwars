13d4c66d17c665517d9abfbbb3a4400b588258ec
Abhigyan103 <abhigyansingh56@gmail.com>
2025-02-11 12:04:04 +0530

    Added robots.txt

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,6 +19,7 @@ export const config = {
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|ico|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         // Always run for API routes
-        '/(api|trpc)(.*)'
+        '/(api|trpc)(.*)',
+        '/robots.txt'
       ],
 }
